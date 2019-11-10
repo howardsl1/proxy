9091372a4d51b30bf3768c4360b65fddf1b3fd70
Sam Howard <howardsl1@appstate.edu>
2019-11-10 15:54:07 -0500

    could do this but not correct

--- WizardTowerProxy.java
+++ WizardTowerProxy.java
@@ -11,22 +11,24 @@ public class WizardTowerProxy implements WizardTower {
     }
 
     public void enter(Wizard wizard) {
+        IvoryTower tower = new IvoryTower();
         if (wizard.getCredits() >= 100 && wiz.size() < 4) {
                 System.out.println(wizard.toString() + " has met the criteria");
                 wiz.add(wizard);
-                //tower.enter(wizard);
+                tower.enter(wizard);
         } else {
             System.out.println("Wizard not allowed in");
         }
     }
 
     public void exit(Wizard wizard) {
+        IvoryTower tower = new IvoryTower();
         boolean c = false;;
         if (wiz.size() > 0) {
             for (int i = 0; i < wiz.size(); i++) {
                 if (wizard.getName().equals(wiz.get(i).getName())) { 
                     System.out.println(wizard.toString() + " has left the tower.");
-                    //tower.exit(wizard);
+                    tower.exit(wizard);
                     wiz.remove(i);
                     c = true;
                 }
